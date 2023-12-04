60aa209853e4a2577081ce5df528b89ed8cd2748
sopheak012 <36690234+sopheak012@users.noreply.github.com>
2023-12-03 21:33:36 -0800

    remove redux and add globalcontext

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { Store } from "@reduxjs/toolkit";
-import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Provider>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
